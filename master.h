a711d2e094f5f00dbbd1f353fdac9a911f39a9e3
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-02-25 17:57:06 -0600

    Create master.h

---
+++ master.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+// Master Function Declarations
+int processMaster(int numberOfChildrenAllowed, int timeInSecondsToTerminate, string InputDataFile);
+int forkProcess(int nItemStart, int nDepth);
