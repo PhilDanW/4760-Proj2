6f32ea40b6b9a94d7430324325c0bae81544a53b
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-02-25 20:39:10 -0600

    Update bin_adder.h

--- bin_adder.h
+++ bin_adder.h
@@ -5,4 +5,4 @@ using namespace std;
 // bin_adder Function Declarations
 static void enterCritical();
 static void allocateSM();
-static void show_usage(std::string);
+static void usage(std::string);
