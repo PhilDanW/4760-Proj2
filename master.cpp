a015fa607b7e29e0c12721eaf1e87fd0b6f3c1a0
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-02-25 21:13:47 -0600

    Update master.cpp

--- master.cpp
+++ master.cpp
@@ -1,13 +1,6 @@
-/********************************************
- * master - Processes and shared memory
- * This file is for the master functionality of the
- * application.  It kicks off from the main file.
- * 
- * Brett Huffman
- * CMP SCI 4760 - Project 2
- * Due Feb 23, 2021
- * Master CPP file for project
- ********************************************/
+/* Philip Wright
+ * master process program that is the workhorse of the application
+ */
 #include <iostream>
 #include <string.h>
 #include <vector>
@@ -16,183 +9,159 @@
 #include <unistd.h>
 #include <errno.h>
 #include "master.h"
-#include "sharedStructures.h"
+#include "shared.h"
 
-// Static process counter => Never > 20 (1 Parent + 19 Children)
 const int MAX_PROCESSES = 19;
-static int ProcessCount = 0;
-const int BUFFERSIZE = 8192;
+static int count = 0;
 
 using namespace std;
 
-// Item Variables
-vector<int> vecItemArray;
-int* addItem_num;
-struct AddItem* addItems;
+//variables to keep track of struct items and info
+vector<int> array;
+int* num;
+struct AddItem* node;
 
 // SIGINT handling
 volatile sig_atomic_t sigIntFlag = 0;
-void sigintHandler(int sig){ // can be called asynchronously
-  sigIntFlag = 1; // set flag
+void sigintHandler(int sig){ 
+  sigIntFlag = 1; //flag
 }
 
-// ProcessMaster
-// The processMaster function to process data from the given input file.
-int processMaster(int numberOfChildrenAllowed, int timeInSecondsToTerminate, string InputDataFile)
+// processes data from the input file.
+int processMaster(int numChildren, int seconds, string dataFile)
 {
     // Register SIGINT handling
     signal(SIGINT, sigintHandler);
-    bool isKilled = false;
+    bool dead = false;
 
     // Start Time for time Analysis
-    time_t secondsStart;
+    time_t start;
 
     // Read in data file
-    FILE *fp;
+    FILE *fileName;
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
 
-    fp = fopen(InputDataFile.c_str(), "r");
-    if (fp == NULL)
+    fileName = fopen(dataFile.c_str(), "r");
+    if (fileName == NULL)
     {
         // Error - cant open file
         errno = ENOENT;
-        perror(InputDataFile.c_str());
+        perror(dataFile.c_str());
         exit(EXIT_FAILURE);
     }
 
-    while ((read = getline(&line, &len, fp)) != -1) {
+    while ((read = getline(&line, &len, fileName)) != -1) {
         int i = atoi(line);
-        vecItemArray.push_back(i);  // Place each line in Vec
+        array.push_back(i);  // Place each line in Vector
     }
-    fclose(fp);
+    fclose(fileName);
     free(line);
 
-    // Get the time in seconds for our process to make
-    // sure we don't exceed the max amount of processing time
-    secondsStart = time(NULL);   // Start time
+    // time in seconds for our process so we dont't exceed the max processing time
+    start = time(NULL);
 
-    // Check that the input file is a power of 2.  Kind of a
-    // wizzy way of doing it... but I like it.  When it's done
-    // it should tell me what power of 2 it is
-    int index = vecItemArray.size();
-    int targetlevel = 0;
-    // Could do this with log(), but then I've got
-    // to use floats. :(
-    while (index >>= 1) ++targetlevel;
+    // Determine power of 2
+    int index = array.size();
+    int level = 0;
+    while (index >>= 1) ++level;
 
-    // While there is less than a full power of 2
-    // Fill in zeros
-    if(pow(2, targetlevel) < vecItemArray.size())
+    // Fill in zeros if not a power of 2
+    if(pow(2, level) < array.size())
     {
-        targetlevel++;
+        level++;
         do
         {
             // Pad Array with zeros
-            vecItemArray.push_back(0);
+            array.push_back(0);
         }
-        while(pow(2, targetlevel) > vecItemArray.size());
+        while(pow(2, level) > array.size());
     }
-
-    // If this was successful, we are finally ready to process
-    // the file adds with our child processes.
-    int arrItemCount = vecItemArray.size();
-
+    // process the file with our child processes.
+    int itemCount = array.size();
+    
     // Allocate the shared memory
-    // And get ready for read/write
     if ((key = ftok(HostProcess, 100)) == -1) {
         perror("ftok");
         exit(EXIT_FAILURE);
     }
-    // allocate a shared memory segment with size of struct array
-    int memSize = sizeof(AddItem) * arrItemCount;
-    shm_id = shmget(key, memSize, IPC_CREAT | IPC_EXCL | 0660);
-    if (shm_id == -1) {
-        perror("shmget: ");
-        exit(EXIT_FAILURE);
+    else {
+        int memSize = sizeof(AddItem) * itemCount;
+        shm_id = shmget(key, memSize, IPC_CREAT | IPC_EXCL | 0660);
+        if (shm_id == -1) {
+            perror("shmget: ");
+            exit(EXIT_FAILURE);
+        }
     }
+  
     // attach the shared memory segment to our process's address space
     shm_addr = (char*)shmat(shm_id, NULL, 0);
     if (!shm_addr) { /* operation failed. */
         perror("shmat: ");
         exit(EXIT_FAILURE);
     }
-    addItem_num = (int*) shm_addr;
-    *addItem_num = 0;
-    addItems = (struct AddItem*) (shm_addr+sizeof(int));
+    num = (int*) shm_addr;
+    *num = 0;
+    node = (struct AddItem*) (shm_addr+sizeof(int));
 
-    // Fill struct
-    for(int i=0; i < arrItemCount; i++)
+    // Fill the struct with values
+    for(int i=0; i < itemCount; i++)
     {
-        addItems[i].itemValue = vecItemArray[i];
-        addItems[i].pidAssigned = 0;
-        addItems[i].complete = false;
-        addItems[i].readyToProcess = true; //(i%2==0);
-        addItems[i].nodeDepth = -1;
-        addItems[i].itemState = idle;
+        node[i].itemValue = array[i];
+        node[i].pidAssigned = 0;
+        node[i].complete = false;
+        node[i].readyToProcess = true; //(i%2==0);
+        node[i].nodeDepth = -1;
+        node[i].itemState = idle;
     }
 
-    // Start Processing with bin_adder xx yy
-    // Where xx is start of processing index and yy is depth.
-    // And the result should just go in the yy spot
-    bool bComplete = false;
-    int status;
-    int arrayIndex = 0;
-    pid_t cpid, waitPID;
+    // Start Processing with bin_adder
+    bool done = false;
+    pid_t waitPID;
     int wstatus;
 
-    // *********** Node Print *****************
-        for(int j=0; j < arrItemCount; j++)
-            cout << "\t" << addItems[j].itemValue;
+    for(int j=0; j < itemCount; j++)
+        cout << "\t" << node[j].itemValue;
         cout << endl;
-    // ****************************************
-
-
-
+    
     // Set a variable to keep track of target level
-    int nDepth = targetlevel;
-    // Now, start looping until the entire calculation is complete
-    // This is in a giant loop that both forks items that need
-    // to be calculated and responds to PID's that are returning/erroring.
-    // Once everything is done, cleanup and exit
-    while(!bComplete)
+    int nDepth = level;
+  
+    // start looping until the entire calculation is complete
+    while(!done)
     {
-        // Search for an "Ready" node first by depth
-        // then by every node in the array
-        // Only do this if we are no Stop Flag, no time to terminate,
-        // not at Max Processing count, and not at max children allowed
-        if(!sigIntFlag && !((time(NULL)-secondsStart) > timeInSecondsToTerminate)
-            && ProcessCount < numberOfChildrenAllowed && ProcessCount < MAX_PROCESSES)
+        // look for Ready node first by depth then by every node in the array
+        if(!sigIntFlag && !((time(NULL)-start) > seconds)
+            && count < numChildren && count < MAX_PROCESSES)
         {
             for(int i=0;i<nDepth;i++)
             {
-                for(int j=0;j<arrItemCount && ProcessCount < numberOfChildrenAllowed && ProcessCount < MAX_PROCESSES
-                    ;j+=pow(2, i+1))
+                for(int j=0; j < itemCount && count < numChildren && count < MAX_PROCESSES; j += pow(2, i+1))
                 {
-                    // j will always be the nodes we need to
-                    // check.  If it is "Ready", check it's 
-                    // partner.  If it's "Ready" too => send to bin_adder
+                    // j isthe nodes we need to check.  
+                    //If process is Ready, check it's partner.  
+                    //If partner is Ready too => send to bin_adder
                     int nCheck1 = j;
                     int nCheck2 = pow(2, i) + j;
 
                     // If the current nodes looked at are ready to process and
                     // haven't already been processed for this depth, process them
-                    if(addItems[nCheck1].nodeDepth < i
-                        && addItems[nCheck1].readyToProcess && addItems[nCheck2].readyToProcess
-                        && addItems[nCheck1].nodeDepth == addItems[nCheck2].nodeDepth)
+                    if(node[nCheck1].nodeDepth < i
+                        && node[nCheck1].readyToProcess && node[nCheck2].readyToProcess
+                        && node[nCheck1].nodeDepth == node[nCheck2].nodeDepth)
                     {
                         // Set as processing
-                        addItems[nCheck1].readyToProcess = addItems[nCheck2].readyToProcess = false;
+                        node[nCheck1].readyToProcess = node[nCheck2].readyToProcess = false;
                         // Set the depth of it's last process run
-                        addItems[nCheck1].nodeDepth = addItems[nCheck2].nodeDepth = i;
+                        node[nCheck1].nodeDepth = node[nCheck2].nodeDepth = i;
                         
                         // Increment our Process Count
-                        ProcessCount++;
+                        count++;
 
                         // Fork and store pid in each node
                         int pid = forkProcess(nCheck1, i);
-                        addItems[nCheck1].pidAssigned = addItems[nCheck2].pidAssigned = pid;
+                        node[nCheck1].pidAssigned = node[nCheck2].pidAssigned = pid;
                     }
                 }
             }
@@ -200,27 +169,22 @@ int processMaster(int numberOfChildrenAllowed, int timeInSecondsToTerminate, str
 
         // Loop through looking for a returning PID
         do {
-            // Note :: We use the WNOHANG to call waitpid without blocking
             // If it returns 0, it does not have a PID waiting
             waitPID = waitpid(-1, &wstatus, WNOHANG | WUNTRACED | WCONTINUED);
 
             // Terminate the process if CTRL-C is typed
-            // or if the max time-to-process has been exceeded
-            // but only send out messages to kill once
-            if((sigIntFlag || (time(NULL)-secondsStart) > timeInSecondsToTerminate) && !isKilled)
+            if((sigIntFlag || (time(NULL)-start) > seconds) && !dead)
             {
-                isKilled = true;
-                // Send signal for every child process to terminate
-                for(int i=0;i<arrItemCount;i++)
+                dead = true;
+                // Send a signal for every child process to terminate
+                for(int i=0;i<itemCount;i++)
                 {
-                    // Send signal to close if they are in-process
-                    if(addItems[i].readyToProcess == false)
-                        kill(addItems[i].pidAssigned, SIGQUIT); 
+                    // Send a signal to close if they are in-process
+                    if(node[i].readyToProcess == false)
+                        kill(node[i].pidAssigned, SIGQUIT); 
                 }
 
                 // We have notified children to terminate immediately
-                // then let program shutdown naturally -- that way
-                // memory is deallocated correctly
                 cout << endl;
                 if(sigIntFlag)
                 {
@@ -238,13 +202,12 @@ int processMaster(int numberOfChildrenAllowed, int timeInSecondsToTerminate, str
             // No PIDs are in-process
             if (waitPID == -1) {
                 // Show finish message only if it's not killed
-                if(!isKilled)
+                if(!dead)
                 {
-                    // Otherwise, the addition is complete.  Show final value
-                    // and gracefully shutdown
-                    int length = snprintf( NULL, 0, "%d", addItems[0].itemValue);
+                    // addition is complete.  Show final value
+                    int length = snprintf( NULL, 0, "%d", node[0].itemValue);
                     char* sDep = (char*)malloc( length + 1 );
-                    snprintf( sDep, length + 1, "%d", addItems[0].itemValue);
+                    snprintf( sDep, length + 1, "%d", node[0].itemValue);
                     string strFinalVal = "*** Addition Process Finished: ";
                     strFinalVal.append(sDep);                    
                     free(sDep);
@@ -256,24 +219,21 @@ int processMaster(int numberOfChildrenAllowed, int timeInSecondsToTerminate, str
                     cout << endl;
                     perror(strFinalVal.c_str());
                 }
-                bComplete = true;   // We say true so that we exit out of main
-                break;              // loop and free up all necessary data
-//                exit(EXIT_FAILURE);
+                done = true;   // We say true so that we exit out of main
+                break;             
             }
 
             if (WIFEXITED(wstatus) && waitPID > 0)
             {
-                // Decrement our ProcessCounter
-                ProcessCount--;
+                // Decrement our process counter
+                count--;
 
                 // Success! Child processed correctly = Show it
-                for(int j=0; j < arrItemCount; j++)
-                    cout << "\t" << addItems[j].itemValue;
+                for(int j=0; j < itemCount; j++)
+                    cout << "\t" << node[j].itemValue;
                 cout << endl;
-                // ****************************************
 
                 // Print out the instance terminated
-                // Make string version of PID
                 int length = snprintf( NULL, 0, "%d", waitPID);
                 char* sDep = (char*)malloc( length + 1 );
                 snprintf( sDep, length + 1, "%d", waitPID );
@@ -284,23 +244,22 @@ int processMaster(int numberOfChildrenAllowed, int timeInSecondsToTerminate, str
                 string strFormattedResult = GetTimeFormatted(strPID.c_str());
                 perror(strFormattedResult.c_str());
 
-                // When the PID that terminates is of 0 depth and it's 0 index
                 // Terminate the entire process => Entire tree has processed
-                if(addItems[0].pidAssigned == waitPID && addItems[0].nodeDepth==nDepth)
+                if(node[0].pidAssigned == waitPID && node[0].nodeDepth==nDepth)
                 {
-                    // Flag to break down the entire memory structure
-                    bComplete = true;
+                    // Flag to break down structure
+                    done = true;
                     break;
                 }
                 else
                 {
                     // Find the item in the array based on PID
-                    for(int i=0;i<arrItemCount;i++)
+                    for(int i=0;i<itemCount;i++)
                     {
-                        if(addItems[i].pidAssigned == waitPID)
+                        if(node[i].pidAssigned == waitPID)
                         {
                             // Set this node as ready to process and continue
-                            addItems[i].readyToProcess = true;
+                            node[i].readyToProcess = true;
                             break;
                         }
                     }
@@ -315,19 +274,19 @@ int processMaster(int numberOfChildrenAllowed, int timeInSecondsToTerminate, str
         } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));        
     }
 
-    // Dedetach shared memory segment from process's address space
+    // Detatch shared memory from process's address space
     cout << endl;
-    perror("De-allocating shared memory");
+    perror("Detatching shared memory");
     if (shmdt(shm_addr) == -1) {
         perror("main: shmdt: ");
     }
 
-    // De-allocate the shared memory segment.
+    // Detatch the shared memory segment.
     if (shmctl(shm_id, IPC_RMID, NULL) == -1) {
         perror("main: shmctl: ");
     }
 
-    perror("Shared memory De-allocated");
+    perror("Shared memory Detatched");
     cout << endl;
 
     // Success!
@@ -335,7 +294,7 @@ int processMaster(int numberOfChildrenAllowed, int timeInSecondsToTerminate, str
 }
 
 
-int forkProcess(int nItemStart, int nDepth)
+int forkProcess(int start, int depth)
 {
         int pid = fork();
         // No child made - exit with failure
@@ -350,23 +309,23 @@ int forkProcess(int nItemStart, int nDepth)
         if(pid == 0)
         {
             // Make string version of nItemStart
-            int length = snprintf( NULL, 0, "%d", nItemStart);
+            int length = snprintf( NULL, 0, "%d", start);
             char* sStart = (char*)malloc( length + 1 );
-            snprintf( sStart, length + 1, "%d", nItemStart );
+            snprintf( sStart, length + 1, "%d", start );
             string strItemStart = sStart;
             free(sStart);
             
             // Make string version of nDepth
-            length = snprintf( NULL, 0, "%d", nDepth);
+            length = snprintf( NULL, 0, "%d", depth);
             char* sDep = (char*)malloc( length + 1 );
-            snprintf( sDep, length + 1, "%d", nDepth );
+            snprintf( sDep, length + 1, "%d", depth );
             string strDepth = sDep;
             free(sDep);
 
             // Execute child process
             execl(ChildProcess, strItemStart.c_str(), strDepth.c_str(), NULL);
 
-            fflush(stdout); // Mostly for debugging -> tty wasn't flushing
+            fflush(stdout);
             exit(EXIT_SUCCESS);    // Exit from forked process successfully
         }
         else
