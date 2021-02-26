03ff4f3381719da4aba6573eacde9e02a57c528a
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-02-25 21:44:33 -0600

    Update shared.h

--- shared.h
+++ shared.h
@@ -1,15 +1,6 @@
-/********************************************
- * sharedStructures - This is a file that
- * contains all the structures shared between
- * the master + bin_adder processes.  It
- * contains library calls, the main structure
- * containing data, and semephores.
- * 
- * Brett Huffman
- * CMP SCI 4760 - Project 2
- * Due Feb 23, 2021
- * sharedStructures .h file for project
- ********************************************/
+/* Philip Wright
+ * shared.h contains shared structures, variables and functions
+ */
 
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -54,8 +45,8 @@ key_t key = 0;  // Shared key
 int shm_id; // Shared Mem ident
 char* shm_addr;
 
-const char* HostProcess = "./master";
-const char* ChildProcess = "./bin_adder";
+const char* Host = "./master";
+const char* Child = "./bin_adder";
 // takes in a string and returns an int
 string GetStringFromInt(const int nVal)
 {
